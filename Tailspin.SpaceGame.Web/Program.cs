571d4cea652914152e233a3b6a43bcd3b40b8cfa
Jari Porra <jari_porra@hotmail.com>
2024-08-07 22:53:56 +0300

    JJP Feature 1 added to Program.cs; Also Index.cshtml updated

--- Tailspin.SpaceGame.Web/Program.cs
+++ Tailspin.SpaceGame.Web/Program.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore;
+﻿// JJP updated Program.cd on 2024-08-07
+using System;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
 namespace TailSpin.SpaceGame.Web
@@ -8,6 +10,11 @@ namespace TailSpin.SpaceGame.Web
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static void JJPNewFunction1(string[] args)
+        {
+            Console.WriteLine("JJP added a function to Progra.cs into feature/myJJPFeature-6 branch");
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
